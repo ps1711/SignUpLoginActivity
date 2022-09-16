541b16738fa4cbf2e4f4724b134515dba91670d2
Priyanshu Singh <singhpriyanshu073@gmail.com>
2022-09-16 17:56:33 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/signuplogininactivity/Signin.java
@@ -0,0 +1,55 @@
+package com.example.signuplogininactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Signin extends AppCompatActivity {
+
+    EditText username,password;
+    Button signInBtn;
+
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        username = findViewById(R.id.Username);
+        password = findViewById(R.id.Password);
+        signInBtn = findViewById(R.id.Login);
+
+        Bundle bundle = getIntent().getExtras();
+
+        String uname = bundle.getString("username");
+        String pwd = bundle.getString("password");
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals(uname) && pass.equals(pwd)){
+                    Toast.makeText(Signin.this, "Success", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    count++;
+                    if (count >= 3) {
+                        signInBtn.setEnabled(false);
+                    } else {
+                        Toast.makeText(Signin.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+
+
+    }
+}
